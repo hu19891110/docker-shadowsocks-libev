78d063c8897144f443d94d2bceefd0fa461c9851
tofuliang <tofuliang@gmail.com>
2017-01-11 01:43:08 +0800

    rewrite daemon script

--- client.sh
+++ client.sh
@@ -5,7 +5,12 @@ if [ "" != "$KCP_SERVER_PORT" ] &&  [ "" != "$KCP_SERVER_ADDR" ];then
 fi
 nohup ss-local -s 127.0.0.1 -p $KCP_LOCAL_PORT  -l $SS_LOCAL_PORT -k $SS_PASSWORD -m $SS_METHOD -t $SS_TIMEOUT -b $SS_LOCAL_ADDR -A --fast-open $SS_OPTIONS ${SS_DEBUG} &
 auth="authorization: Basic `echo $ARUKAS_TOKEN:$ARUKAS_SECERT|tr -d "\n" |base64|tr -d "\n"`"
-api=https://app.arukas.io/api/containers
+
+listContainerApi=https://app.arukas.io/api/containers
+createAppApi=https://app.arukas.io/api/app-sets
+listAppApi=https://app.arukas.io/api/apps
+
+DOCKER_IMAGE="tofuliang/docker-shadowsocks-libev:latest"
 _kcpServerPort=x
 _kcpUdpPortIndex=x
 _ssServerPort=x
@@ -13,31 +18,112 @@ _ssTcpPortIndex=x
 
 COW_DEBUG=""
 SS_DEBUG=""
+WGET_DEBUG="-q"
 [ "$DEBUG" == "true" ] && COW_DEBUG="-debug"
 [ "$DEBUG" == "true" ] && SS_DEBUG="-v"
+[ "$DEBUG" == "true" ] && WGET_DEBUG="-d -v "
+
+noContainerCount=0
+noPortCount=0
 
 query() {
-    wget --quiet \
+    wget ${WGET_DEBUG} \
     --no-check-certificate \
     --method $1 \
-    --header "'content-type: application/vnd.api+json'" \
-    --header "'accept: application/vnd.api+json'" \
+    --header 'content-type: application/vnd.api+json' \
+    --header 'accept: application/vnd.api+json' \
     --header "$2" \
-    --header "'cache-control: no-cache'" \
+    --header 'cache-control: no-cache' \
+    --body-data "$4" \
     --output-document \
     - $3
 }
 
-while true; do
-    restart=false
-    json=$(query "GET" "$auth" "$api")
-    _containerId=$(echo $json|jq '.data[0].id'|sed 's/"//g')
-    _envs=$(echo $json|jq '.data[0].attributes.envs')
+createApp(){
+    echo "[EVENT] creating app..."
+    query "POST" "$auth" "$createAppApi" "{\"data\": [{\"type\": \"containers\", \"attributes\": {\"image_name\": \"${DOCKER_IMAGE}\", \"instances\": 1, \"mem\": 512, \"cmd\": \"\", \"envs\": [{\"key\": \"SS_SERVER_PORT\", \"value\": \"${SS_SERVER_PORT}\"}, {\"key\": \"SS_PASSWORD\", \"value\": \"${SS_PASSWORD}\"}, {\"key\": \"KCP_SERVER_PORT\", \"value\": \"${KCP_SERVER_PORT}\"}, {\"key\": \"KCP_CRYPT\", \"value\": \"${KCP_CRYPT}\"}, {\"key\": \"KCP_DSCP\", \"value\": \"${KCP_DSCP}\"}, {\"key\": \"KCP_OPTIONS\", \"value\": \"${KCP_OPTIONS}\"}, {\"key\": \"KCP_MTU\", \"value\": \"${KCP_MTU}\"} ], \"ports\": [{\"number\": ${SS_SERVER_PORT}, \"protocol\": \"tcp\"}, {\"number\": ${KCP_SERVER_PORT}, \"protocol\": \"udp\"} ]} }, {\"type\": \"apps\", \"attributes\": {\"name\": \"ss-kcp\"} } ] }"
+}
+
+powerUpContainer(){
+    containerId=$(getContainerId)
+    powerApi="https://app.arukas.io/api/containers/$containerId/power"
+    echo "[EVENT] starting container..."
+    query "POST" "$auth" "$powerApi"
+    sleep 3
+    echo "[EVENT] stopping container..."
+    query "DELETE" "$auth" "$powerApi"
+    sleep 3
+    echo "[EVENT] starting container..."
+    query "POST" "$auth" "$powerApi"
+
+}
+
+deleteApps(){
+    json=$(query "GET" "$auth" "$listAppApi")
+    _apps=$(echo $json|jq '.data')
+    _appCount=$(echo $_apps|jq '.|length')
+    for i in `seq 0 $_appCount`;
+    do
+        _appName=$(echo $_apps|jq '.['$i'].attributes.name'|sed 's/"//g')
+        if [ "$_appName" = "ss-kcp" ] ; then
+            _appId=$(echo $_apps|jq '.['$i'].id'|sed 's/"//g')
+            deleteApi="https://app.arukas.io/api/apps/$_appId"
+            echo "[EVENT] deleteing app... [$_appId]"
+            query "DELETE" "$auth" "$deleteApi"
+        fi
+    done
+}
+getContainerId(){
+#    echo "[EVENT] getting containerId..."
+    json=$(query "GET" "$auth" "$listAppApi")
+    _apps=$(echo $json|jq '.data')
+    _appCount=$(echo $_apps|jq '.|length')
+    _containerId=""
+    for i in `seq 0 $_appCount`;
+    do
+        _appName=$(echo $_apps|jq '.['$i'].attributes.name'|sed 's/"//g')
+        if [ "$_appName" = "ss-kcp" ] ; then
+                _containerId=$(echo $_apps|jq '.['$i'].relationships.container.data.id'|sed 's/"//g')
+        fi
+    done
+    echo $_containerId
+}
+getContainerInfo(){
+#    echo "[EVENT] getting containerInfo..."
+    json=$(query "GET" "$auth" "$listContainerApi")
+    _containers=$(echo $json|jq '.data')
+    _containerCount=$(echo $_containers|jq '.|length')
+    _containerInfo=""
+    for i in `seq 0 $_containerCount`;
+    do
+        _containerId=$(echo $json|jq '.data['$i'].id'|sed 's/"//g')
+        if [ "$_containerId" = "$1" ] ; then
+                _containerInfo=$(echo $json|jq '.data['$i']')
+        fi
+    done
+    echo $_containerInfo
+
+}
+
+resetGfwApp(){
+    echo "[EVENT] resetGfwApp ..."
+
+    containerId=$(getContainerId)
+    echo "[EVENT] got containerId ... $containerId"
+
+    [ "$containerId" = "" ] && noContainerCount=$(($noContainerCount + 1))
+
+    echo "noContainerCount=$noContainerCount"
+
+    containerInfo=$(getContainerInfo "$containerId")
+    echo "[EVENT] got containerInfo ... $containerInfo"
+
+    _envs=$(echo $containerInfo|jq '.attributes.envs')
     _envCount=$(echo $_envs |jq '.|length' )
-    _ports=$(echo $json|jq '.data[0].attributes.ports')
+    _ports=$(echo $containerInfo|jq '.attributes.ports')
     _portCount=$(echo $_ports |jq '.|length' )
-    _portMappings=$(echo $json|jq '.data[0].attributes.port_mappings')
-    _updateAt=$(echo $json|jq '.data[0].attributes.updated_at')
+    _portMappings=$(echo $containerInfo|jq '.attributes.port_mappings')
+    _updateAt=$(echo $containerInfo|jq '.attributes.updated_at')
     _updateDate=$(echo $_updateAt|sed 's/"//g'|awk -FT '{print $1}')
     _updateTimeStamp=0
     [ "$_updateDate" != "" ] && _updateTimeStamp=$(date -d $_updateDate +%s)
@@ -66,9 +152,14 @@ while true; do
         fi
     done
 
-    open_udp_port=$(echo $json|jq '.data[0].attributes.port_mappings[0]['$_kcpUdpPortIndex'].service_port'|sed 's/"//g')
-    open_host=$(echo $json|jq '.data[0].attributes.port_mappings[0]['$_kcpUdpPortIndex'].host'|sed 's/"//g')
-    open_tcp_port=$(echo $json|jq '.data[0].attributes.port_mappings[0]['$_ssTcpPortIndex'].service_port'|sed 's/"//g')
+    open_udp_port=$(echo $containerInfo|jq '.attributes.port_mappings[0]['$_kcpUdpPortIndex'].service_port'|sed 's/"//g')
+    open_host=$(echo $containerInfo|jq '.attributes.port_mappings[0]['$_kcpUdpPortIndex'].host'|sed 's/"//g')
+    open_tcp_port=$(echo $containerInfo|jq '.attributes.port_mappings[0]['$_ssTcpPortIndex'].service_port'|sed 's/"//g')
+
+    [ "$open_udp_port" = "null" ] && [ "$containerId" != "" ] && noPortCount=$(($noPortCount + 1))
+    echo "noPortCount=$noPortCount"
+
+    restart=false
 
     [ "$SS_SERVER_PORT" != "$open_tcp_port" ] && [ "null" != "$open_tcp_port" ] && [ "" != "$open_tcp_port" ] && SS_SERVER_PORT=$open_tcp_port && restart=true && export SS_SERVER_PORT
     [ "$KCP_SERVER_PORT" != "$open_udp_port" ] && [ "null" != "$open_udp_port" ] && [ "" != "$open_udp_port" ] && KCP_SERVER_PORT=$open_udp_port && restart=true && export KCP_SERVER_PORT
@@ -97,14 +188,41 @@ while true; do
         && echo "proxy = ss://${SS_METHOD}-auth:${SS_PASSWORD}@${KCP_SERVER_ADDR}:${SS_SERVER_PORT}" >> /etc/cow/rc.run
         nohup cow -rc=/etc/cow/rc.run ${COW_DEBUG} -logFile=/dev/stdout -listen=http://${COW_LOCAL_ADDR}:${COW_LOCAL_PORT} &
     fi
+
+    if [ $noPortCount -gt 5 ];then
+        deleteApps
+        echo $(createApp)
+        sleep 5
+        echo $(powerUpContainer)
+        sleep 10
+        noPortCount=0
+        noContainerCount=0
+    fi
+
+    if [ $noContainerCount -gt 5 ];then
+        deleteApps
+        echo $(createApp)
+        sleep 5
+        echo $(powerUpContainer)
+        sleep 10
+        noPortCount=0
+        noContainerCount=0
+    fi
+
     if [ $(($_updateTimeStamp - 1482364800)) -gt 0 ] && [ $((`date -u +%s` - $_updateTimeStamp)) -gt 86400 ] && [ $((`date -u +%H` + 8 - 3)) -gt 0 ];then
-        powerApi="https://app.arukas.io/api/containers/$_containerId/power"
-        echo "[EVENT] "`date`
-        echo "[EVENT] stopping container..."
-        query "DELETE" "$auth" "$powerApi"
-        sleep 3
-        echo "[EVENT] starting container..."
-        query "POST" "$auth" "$powerApi"
+        deleteApps
+        echo $(createApp)
+        sleep 5
+        echo $(powerUpContainer)
+        sleep 10
+        noPortCount=0
+        noContainerCount=0
     fi
     sleep $ARUKAS_CHECK_FEQ
+
+}
+
+echo $auth
+while true; do
+    resetGfwApp
 done
